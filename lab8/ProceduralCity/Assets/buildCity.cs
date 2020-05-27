a4a9ba2d181e109bcdd662bcb4c065474a79377f
Max Gasser <mrgasser@ucsc.edu>
2020-05-26 19:55:11 -0700

    adding finished tutorial

--- lab8/ProceduralCity/Assets/buildCity.cs
+++ lab8/ProceduralCity/Assets/buildCity.cs
@@ -11,22 +11,26 @@ public class buildCity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        float seed = Random.Range(0, 100);
         for(int h = 0; h < mapHeight; h++)
         {
             for(int w = 0; w < mapWidth; w++)
             {
                 Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
-                int n = Random.Range(0,buildings.Length);
-                Instantiate(buildings[n], pos, Quaternion.identity);
+                int result = (int) (Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
+                if(result < 2)
+                  Instantiate(buildings[0], pos, Quaternion.identity);
+                else if(result < 4)
+                  Instantiate(buildings[1], pos, Quaternion.identity);
+                else if(result < 6)
+                  Instantiate(buildings[2], pos, Quaternion.identity);
+                else if(result < 7)
+                  Instantiate(buildings[3], pos, Quaternion.identity);
+                else if(result < 10)
+                  Instantiate(buildings[4], pos, Quaternion.identity);
             }
         }
 
     }
-    /*
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-    */
 }
